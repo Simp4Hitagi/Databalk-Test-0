52270fa58660ef16bb01a3fabc61d80bb037ce62
Simp4Hitagi <denorauten@gmail.com>
2023-06-02 21:52:38 +0200

    added model functionality for users and houses, created controllers directory with routes, need to add login/register and middleware and authentication and specify processes in README file

---
+++ controllers/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const route = express.Router();
+const {User, Houses} = require('../models/index');
+
+const user = new User();
+const house = new Houses();
+
+// ===Users====
+
+// Retrieve all users
+route.get('/users', (req, res)=>{
+    user.fetchUsers(req, res);
+});
+// Retrieve single user
+route.get('/user/:userID', (req, res)=>{
+    user.fetchUser(req, res);
+});
+// Login
+// route.post('/login', bodyParser.json(),(req, res)=>{
+//     user.login(req, res);
+// });
+// Register
+// route.post('/register', bodyParser.json(), (req, res)=> {
+//     user.createUser(req, res);
+// })
+
+// Update
+route.put('/user/:userID',bodyParser.json(), (req, res)=>{
+    user.updateUser(req, res);
+});
+// Delete
+route.delete('/user/:userID', (req, res)=>{
+    user.deleteUser(req, res);
+});
+// =====Houses======
+// Fetch all houses
+route.get('/houses', (req, res)=> {
+    house.fetchHouses(req, res);
+})
+// Fetch a single house
+route.get('/house/:houseID',
+(req, res)=> {
+    house.fetchHouse(req, res);
+})
+// Add a new house
+route.post('/house', 
+bodyParser.json(), 
+(req, res)=> {
+    house.addHouse(req, res);
+})
+// Update a house
+route.put('/house/:houseID', 
+bodyParser.json(),
+(req, res)=> {
+    house.updateHouse(req, res);
+})
+// Delete a house
+route.delete('/house/:houseID',
+(req, res)=> {
+    house.deleteHouse(req, res);
+})
+
+module.exports = route;
---
+++ models/index.js
@@ -0,0 +1,232 @@
+const database = require('../config/database');
+// bcrypt module
+const {hash, compare, hashSync } = require('bcrypt');
+// Middleware for creating a token
+const {createToken} = require('../middleware/AuthenticatedUser');
+
+// User
+class User {
+    // login(req, res) {
+    //     const {emailAddress, userPassword} = req.body;
+    //     const strQry = 
+    //     `
+    //     SELECT userName, emailAddress, userPassword
+    //     FROM Users
+    //     WHERE emailAddress = '${emailAddress}';
+    //     `;
+    //     database.query(strQry, async (err, data)=>{
+    //         if(err) throw err;
+    //         if((!data.length) || (data == null)) {
+    //             res.status(401).json({err: 
+    //                 "You provided the wrong email address"});
+    //         }else {
+    //             await compare(userPassword, 
+    //                 data[0].userPassword, 
+    //                 (cErr, cResult)=> {
+    //                     if(cErr) throw cErr;
+    //                     // Create a token
+    //                     const jwToken = 
+    //                     createToken(
+    //                         {
+    //                             emailAddress, userPassword
+    //                         }
+    //                     );
+    //                     // Saving
+    //                     res.cookie('LegitUser',
+    //                     jwToken, {
+    //                         maxAge: 3600000,
+    //                         httpOnly: true,
+    //                         path: '/'
+    //                     })
+    //                     if(cResult) {
+    //                         res.status(200).json({
+    //                             msg: 'Logged in',
+    //                             jwToken,
+    //                             result: data[0]
+    //                         })
+    //                     }else {
+    //                         res.status(401).json({
+    //                             err: 'You entered an invalid password or you are not registered. '
+    //                         })
+    //                     }
+    //                 })
+    //         }
+    //     })     
+    // }
+    fetchUsers(req, res) {
+        const strQry = 
+        `
+        SELECT userName, emailAddress, userPassword
+        FROM Users;
+        `;
+        //database
+        database.query(strQry, (err, data)=>{
+            if(err) console.log(err);
+            else res.status(200).json( 
+                {results: data} );
+        })
+    }
+    fetchUser(req, res) {
+        const strQry = 
+        `
+        SELECT userID, userName, emailAddress
+        FROM Users
+        WHERE userID = ?;
+        `;
+        //db
+        database.query(strQry,[req.params.userID], 
+            (err, data)=>{
+            if(err) throw err;
+            else res.status(200).json( 
+                {results: data} );
+        })
+
+    }
+    async createUser(req, res) {
+        // Payload
+        let details = req.body;
+        // Hashing user password
+        details.userPassword = await 
+        hash(details.userPassword, 15);
+        // This information will be used for authentication.
+        let user = {
+            emailAddress: details.emailAddress,
+            userPassword: details.userPassword
+        }
+        // sql query
+        const strQry =
+        `INSERT INTO Users
+        SET ?;`;
+        database.query(strQry, [details], (err)=> {
+            if(err) {
+                res.status(401).json({err});
+            }else {
+                // Create a token
+                const jwToken = createToken(user);
+                // This token will be saved in the cookie. 
+                // The duration is in milliseconds.
+                res.cookie("LegitUser", jwToken, {
+                    maxAge: 3600000,
+                    httpOnly: true,
+                    path: '/'
+                });
+                res.status(200).json({msg: "A user record was saved."})
+            }
+        })    
+    }
+    updateUser(req, res) {
+        let data = req.body;
+        if(data.userPassword !== null || 
+            data.userPassword !== undefined)
+            data.userPassword = hashSync(data.userPassword, 15);
+        const strQry = 
+        `
+        UPDATE Users
+        SET ?
+        WHERE userID = ?;
+        `;
+        //database
+        database.query(strQry,[data, req.params.userID], 
+            (err)=>{
+            if(err) throw err;
+            res.status(200).json( {msg: 
+                "A row was affected"} );
+        })    
+    }
+    deleteUser(req, res) {
+        const strQry = 
+        `
+        DELETE FROM Users
+        WHERE userID = ?;
+        `;
+        //database
+        database.query(strQry,[req.params.userID], 
+            (err)=>{
+            if(err) throw err;
+            res.status(200).json( 
+                {msg: 
+                    "A record was removed from a database"
+            } );
+        })    
+    }
+}
+
+// Houses
+class Houses {
+    fetchHouses(req, res) {
+        const strQry = `SELECT houseID, houseDescription, 
+        location, price, imgURL
+        FROM Houses;`;
+        database.query(strQry, (err, results)=> {
+            if(err) throw err;
+            res.status(200).json({results: results})
+        });
+    }
+    fetchHouse(req, res) {
+        const strQry = `SELECT houseID, houseDescription, 
+        location, price, imgURL
+        FROM Houses
+        WHERE houseID = ?;`;
+        database.query(strQry, [req.params.houseID], (err, results)=> {
+            if(err) throw err;
+            res.status(200).json({results: results})
+        });
+
+    }
+    addHouse(req, res) {
+        const strQry = 
+        `
+        INSERT INTO Houses
+        SET ?;
+        `;
+        database.query(strQry,[req.body],
+            (err)=> {
+                if(err){
+                    res.status(400).json({err: "Unable to insert a new record."});
+                }else {
+                    res.status(200).json({msg: "House saved"});
+                }
+            }
+        );    
+
+    }
+    updateHouse(req, res) {
+        const strQry = 
+        `
+        UPDATE Houses
+        SET ?
+        WHERE houseID = ?
+        `;
+        database.query(strQry,[req.body, req.params.houseID],
+            (err)=> {
+                if(err){
+                    res.status(400).json({err: "Unable to update a record."});
+                }else {
+                    res.status(200).json({msg: "House updated"});
+                }
+            }
+        );    
+
+    }
+    deleteHouse(req, res) {
+        const userId = req.user.userID;
+        const houseId = req.params.houseID;
+        const strQry = `DELETE FROM Houses WHERE houseID = ? AND userID = ?`;
+        database.query(strQry, [houseId, userId], (err, result) => {
+          if (err) throw err;
+          if (result.affectedRows === 0) {
+            return res.status(404).json({ error: "House not found or not authorized" });
+          }
+          res.status(204).end();
+        });
+      }
+      
+
+}
+
+
+// Exporting All classes
+module.exports = {
+    User,
+    Houses
+}
